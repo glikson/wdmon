5f9305cf3a188be8d4d2bd0aad6254e004deea7a
Alex Glikson <alex.glikson@gmail.com>
2025-01-21 13:30:19 +0200

    Distinguish between OOM and non-graceful termination.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # wdmon - Kubernetes Workload Disruption Monitor
 
-## About this Python App
-This tool monitors Kubernetes Pods to detect containers terminated by SIGKILL rather than the graceful SIGTERM, helping identify unexpected disruptions in workloads.
+This tool monitors Kubernetes Pods, to detect containers terminated by SIGKILL rather than the graceful SIGTERM, helping identify unexpected disruptions in workloads. The goal is to distinguish between unexpected terminations due to OOM and terminations due to termination grace period expiration.
+
+## How to test
+Use the attached wdtest.yaml to run the test Deployment:
+
+```
+kubectl create -f wdtest.yaml
+```
+
+Test SIGTERM due to nongraceful termination by terminating the pod created by the Deployment (using kubectl):
+```
+kubectul delete pod -l app=wdtest
+```
+
+Test SIGTERM due to OOM by triggering memory-intesive logic in the container, by creating a file via executing a 'touch' command:
+```
+kubectl exec deploy/wdtest -- touch /tmp/oom
+```
--- wdmon.py
+++ wdmon.py
@@ -1,21 +1,25 @@
 import time
 from kubernetes import client, config, watch
-import logging
-
-logging.basicConfig(level=logging.INFO)
 
 def main():
     config.load_kube_config()
     v1 = client.CoreV1Api()
     w = watch.Watch()
     for event in w.stream(v1.list_pod_for_all_namespaces):
+#        print(".", end="", flush=True)
         pod = event["object"]
         if event["type"] == "MODIFIED":
-            if pod.status.phase == "Failed":
-                for cs in pod.status.container_statuses or []:
-                    if cs.state.terminated and cs.state.terminated.exit_code == 137:
-                        logging.warning(f"Container {cs.name} in {pod.metadata.name} exited with 137 (possible SIGKILL).")
-            print(f"Checking {pod.metadata.name} for abrupt container stops...")
-
+            for cs in pod.status.container_statuses or []:
+                # Check that the container has been SIGKILLed
+                if cs.state.terminated and cs.state.terminated.exit_code == 137:
+                    if cs.state.terminated.reason == "OOMKilled":
+                        print(f"Container {cs.name} in {pod.metadata.name} exited with 137 (OOMKilled).")
+                    elif cs.state.terminated.reason == "Error":
+                        # If the pod has deletion time and deletion grace period of 0, print a message. 
+                        # Otherwise ignore (non-graceful termination involves several Pod updates).
+                        # TODO: This is a heuristic and may not work in all cases.
+                        if pod.metadata.deletion_timestamp and pod.metadata.deletion_grace_period_seconds == 0:
+                            print(f"Container {cs.name} in {pod.metadata.name} exited with 137 (Error) - likely due to non-graceful termination.")
+ 
 if __name__ == "__main__":
     main()
